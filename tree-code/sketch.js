dee20a07fae1d5ef74df0abcdf2a15f0f22832a9
Diego <dhurtad2@uci.edu>
2020-09-25 07:26:55 -0700

    working version

--- tree-code/sketch.js
+++ tree-code/sketch.js
@@ -2,45 +2,87 @@
 // axiom: A
 // rules: (A -> AB), (B -> A)
 
+var angle;
+var sentence = "X";
+var len = 200;
+var rules = [];
 
-var sentence = "A";
 
-var rule1 = {
-    a: "A",
-    b: "AB"
+rules[0] = {
+    a: "X",
+    b: "F+[[X]-X]-F[-FX]+X"
 }
 
-var rule2 = {
-    a: "B",
-    b: "A"
+rules[1] = {
+    a: "F",
+    b: "FF"
 }
 
 function generate() { 
     var nextSentence = "";
+    len *= 0.51;
+    
     for(var i = 0; i < sentence.length; i++) {
         var current = sentence.charAt(i);
-        if (current == rule1.a) {
-            nextSentence += rule1.b;
+        var found = false;
+        
+        for(var j = 0; j < rules.length; j++) {
+            if(current == rules[j].a) {
+                found = true;
+                nextSentence += rules[j].b;
+                break;
+            }
         }
-        else if(current == rule2.a) {
-            nextSentence += rule2.b;
-        }
-        else {
+        
+        if(!found){
             nextSentence += current;
         }
+        
     }
     
     sentence = nextSentence;
     createP(sentence);
+    mover();
+}
+
+function mover() {    
+    background(0);
+    resetMatrix();
+    translate(width / 2, height);
+    stroke(255);    
+        
+    for(var i = 0; i < sentence.length; i++) {
+        var current = sentence.charAt(i);
+        
+        if(current == "F"){
+            line(0, 0, 0, -len);
+            translate(0, -len);
+        }
+        else if(current == "+") {
+            rotate(angle);
+        }
+        else if(current == "-") {
+            rotate(-angle);
+        }
+        else if(current == "[") {
+            push();
+        }
+        else if(current == "]") {
+            pop();
+        }
+    }
 }
 
 function setup() {
-    noCanvas();
+    createCanvas(600,600);
+    angle = radians(25);
+    background(0);
     createP(sentence);
+    mover();
     var button = createButton("Generate Sentence");
     button.mousePressed(generate);
 }
 
-function draw() {
-  
+function draw() {  
+    
 }
